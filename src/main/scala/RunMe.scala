a314880a4e90bf6abef2b4d36c972a056de72b82
ScalaWilliam <william@scalawilliam.com>
2018-10-28 02:18:26 +0200

    This works well.

--- src/main/scala/RunMe.scala
+++ src/main/scala/RunMe.scala
@@ -4,6 +4,8 @@ import org.graalvm.polyglot.proxy._
 
 object RunMe {
 
+  final case class Wah[T](x: T)
+
   def main(args: Array[String]): Unit = {
     println("Hello Java!")
     val item = new MySomething {
@@ -16,5 +18,9 @@ object RunMe {
     val r = x.getMember("result").asInt()
     println(s"Result = $r")
     context.eval("js", "print('Hello JavaScript!');")
+
+    val f2 = context.eval("js", "function(x) (x.copy(2))")
+    val r2 = f2.execute(Wah("Hey"))
+    println(s"Result = ${r2}")
   }
 }
